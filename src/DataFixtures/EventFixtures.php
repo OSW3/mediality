962de4689ceed921541808dd16c2bec5cf75e3b0
Julien Deudon <deudon.j@gmail.com>
2019-04-15 14:19:44 +0200

    fixtures perfect

--- src/DataFixtures/EventFixtures.php
+++ src/DataFixtures/EventFixtures.php
@@ -57,32 +57,31 @@ class EventFixtures extends Fixture
                          ->setDateDelivery($faker->dateTimeBetween($minimum));
 
                     $manager->persist($commande);
-
-                for ($k=0; $k <= 3 ; $k++) { 
-                    $team = new Team();
-                    $team->setName($faker->randomElement([
-                        'Internet', 'Télévision', 'Radio', 'Journal'
-                    ]))
-                        ->setRole("Pizza")
-                        ->setDescription($faker->sentence(46));
-
-                    $manager->persist($team);
-
-                    for ($l=0; $l < 6; $l++) { 
-                        $user = new Users();
-                        $user->setFirstName($faker->firstName)
-                             ->setLastName($faker->lastName)
-                             ->setEmail($faker->freeEmail)
-                             ->setPhone($faker->phoneNumber)
-                             ->setStatus('Journaliste')
-                             ->setPassword('testtest');
-
-                        $manager->persist($user);
-                    }
-                }
-
             }
 
+        }
+
+        for ($k=0; $k <= 3 ; $k++) { 
+            $team = new Team();
+            $team->setName($faker->randomElement([
+                'Internet', 'Télévision', 'Radio', 'Journal'
+            ]))
+                ->setRole("Pizza")
+                ->setDescription($faker->sentence(46));
+
+            $manager->persist($team);
+
+            for ($l=0; $l < 6; $l++) { 
+                $user = new Users();
+                $user->setFirstName($faker->firstName)
+                     ->setLastName($faker->lastName)
+                     ->setEmail($faker->freeEmail)
+                     ->setPhone($faker->phoneNumber)
+                     ->setStatus('Journaliste')
+                     ->setPassword('testtest');
+
+                $manager->persist($user);
+            }
         }
 
 
