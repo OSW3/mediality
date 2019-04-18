0035fa83109af65acce221f75d82c0a97847cc68
Goetbloet Gregory <g.goetbloet@gmail.com>
2019-04-18 13:33:10 +0100

    integration 2eme

---
+++ public/assets/js/calendar/sideBar.js
@@ -0,0 +1,15 @@
+var btn = document.querySelector('.toggle_btn');
+var nav = document.querySelector('.sidebar');
+var corp = document.querySelector('.spaceCalendar');
+var filter = document.querySelector('.toggle_btn2-open');
+
+
+btn.onclick=function(){
+    nav.classList.toggle('sidebar-hidden');
+
+    corp.classList.toggle('spaceCalendar-hidden');
+
+    filter.classList.toggle('toggle_btn2-close');
+
+}
+
