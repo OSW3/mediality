a7219b578d27026f1edad31e4bb227bc347929bc
Larsouille <helloktana@gmail.com>
2019-04-25 12:05:44 +0200

    Modifs twig / CSS

--- src/Migrations/Version20190424130423.php
+++ src/Migrations/Version20190424130423.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190418150245 extends AbstractMigration
+final class Version20190424130423 extends AbstractMigration
 {
     public function getDescription() : string
     {
